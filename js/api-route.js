19d342b922f6b1a419103ddfeb8aff2bc545575f
kris <kris@gmail.com>
2021-03-21 02:47:13 +0700

    testing cors

--- js/api-route.js
+++ js/api-route.js
@@ -1,14 +1,24 @@
 function shortener(url) {
-    $.post('https://cleanuri.com/api/v1/shorten', {
-        'url': url
-    }, function(res){
-        console.log("sadsad")
-        console.log(res)
-    });
     $.ajax({
         type: "GET",
         url: 'https://api.kawalcorona.com/indonesia',
-        // dataType:"jsonp",
+        dataType: 'jsonp',
+        cors: true ,
+        contentType:'application/json',
+        success: function(res){
+                console.log("sadsad")
+                console.log(res)
+        }
+      });
+    $.ajax({
+        type: "POST",
+        url: 'https://cleanuri.com/api/v1/shorten',
+        dataType: 'jsonp',
+        cors: true ,
+        contentType:'application/json',
+        data: {
+            'url': url
+        },
         success: function(res){
                 console.log("sadsad")
                 console.log(res)
